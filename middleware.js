40a4649b23b7188ca81c02f0ba319a1149c25d3e
hosseinabadii <khosro.h2011@gmail.com>
2025-02-10 00:29:17 +0330

    Add movie details page to middleware

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api/(.*)",
-  "/favorites",
+  "/movie/(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
